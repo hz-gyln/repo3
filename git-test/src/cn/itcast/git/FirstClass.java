28f6105ddf3061c9a6819a50dd3da5f3531cf587
hz <2804956490@qq.com>
2019-07-10 21:10:50 +0800

    工程中添加了一个方法，再把工程到版本库

--- git-test/src/cn/itcast/git/FirstClass.java
+++ git-test/src/cn/itcast/git/FirstClass.java
@@ -5,4 +5,12 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello Git");
     }
+
+    public void test(){
+
+
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
 }
