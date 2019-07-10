6317fb10abc782e2f75c399c138ebb3435e83bf2
hz <2804956490@qq.com>
2019-07-10 21:21:53 +0800

    添加了注释

--- git-test/src/cn/itcast/git/FirstClass.java
+++ git-test/src/cn/itcast/git/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello Git");
+
+        //调用方法
     }
 
     public void test(){
@@ -12,5 +14,7 @@ public class FirstClass {
         int i = 0;
         i += 10;
         System.out.println(i);
+
+        System.out.println(i+100);
     }
 }
